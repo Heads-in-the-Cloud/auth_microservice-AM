f4bde8b3230da9830d67d6143c80b04f4052ddf5
aidan.mattson@smoothstack.com <aidan.mattson@wsu.edu>
2021-12-16 18:01:14 -0800

    Removed SSL for EKS contexts

--- src/main/java/com/ss/training/utopia/auth/AuthApplication.java
+++ src/main/java/com/ss/training/utopia/auth/AuthApplication.java
@@ -14,35 +14,5 @@ import org.springframework.context.annotation.Bean;
 public class AuthApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuthApplication.class, args);
-        System.out.println();
-    }
-
-    @Bean
-    public ServletWebServerFactory servletContainer() {
-        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
-            @Override
-            protected void postProcessContext(Context context) {
-                SecurityConstraint securityConstraint = new SecurityConstraint();
-                securityConstraint.setUserConstraint("CONFIDENTIAL");
-                SecurityCollection collection = new SecurityCollection();
-                collection.addPattern("/*");
-                securityConstraint.addCollection(collection);
-                context.addConstraint(securityConstraint);
-            }
-        };
-
-        tomcat.addAdditionalTomcatConnectors(httpToHttpsRedirectConnector());
-
-        return tomcat;
-    }
-
-    private Connector httpToHttpsRedirectConnector() {
-        // redirect from old (http) port 8082 to secure (https) port 8443
-        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
-        connector.setScheme("http");
-        connector.setPort(8085);
-        connector.setSecure(false);
-        connector.setRedirectPort(8443);
-        return connector;
     }
 }
